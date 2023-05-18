7ef25cfb68ae9aeda9cd7f42400e8ee5f8abebeb
MathiasChirivi <chirivimathias3@gmail.com>
2023-05-18 16:46:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+
+
+export default {
+  components: {
+    AppTitle,
+  }
+}
+</script>
+
+<template>
+  <header>
+    <AppTitle />
+
+  </header>
+
+  <main>
+
+  </main>
+</template>
+
+<style scoped></style>
